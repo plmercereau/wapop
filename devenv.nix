21ec1f82588f3f786b71406efcc614bded0b2666
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2025-01-20 22:18:34 +0100

    sys

--- devenv.nix
+++ devenv.nix
@@ -17,14 +17,15 @@
           if pkgs.stdenv.hostPlatform.isAarch64
           then "arm64"
           else "amd64";
+        system = "${os}_${arch}";
         hashes = {
-          "darwin-arm64" = "sha256-Nf+tZJU0NejmbjuAJdUtwhOrCPimw/7Z6PwNlve8nIQ=";
-          "linux-amd64" = "";
+          "darwin_arm64" = "sha256-Nf+tZJU0NejmbjuAJdUtwhOrCPimw/7Z6PwNlve8nIQ=";
+          "linux_amd64" = "";
         };
       in
         pkgs.fetchurl {
-          url = "https://github.com/operator-framework/operator-sdk/releases/download/v1.38.0/operator-sdk_${os}_${arch}";
-          sha256 = hashes."${os}-${arch}";
+          url = "https://github.com/operator-framework/operator-sdk/releases/download/v1.38.0/operator-sdk_${system}";
+          sha256 = hashes.${system};
         };
       phases = ["installPhase" "patchPhase"];
       installPhase = ''
@@ -37,17 +38,15 @@
     helmify = pkgs.stdenv.mkDerivation {
       name = "helmify";
       src = let
-        os = pkgs.stdenv.hostPlatform.uname.system;
-        arch = pkgs.stdenv.hostPlatform.linuxArch;
-
+        system = "${pkgs.stdenv.hostPlatform.uname.system}_${pkgs.stdenv.hostPlatform.linuxArch}";
         hashes = {
-          "Darwin-arm64" = "sha256-yelo1YhTk/Zj0C2w64ii1xDOTdUuMSNV5+Sk6ZXlsoc=";
-          "Linux-amd64" = "";
+          "Darwin_arm64" = "sha256-yelo1YhTk/Zj0C2w64ii1xDOTdUuMSNV5+Sk6ZXlsoc=";
+          "Linux_x86_64" = "";
         };
       in
         pkgs.fetchurl {
-          url = "https://github.com/arttor/helmify/releases/download/v0.4.17/helmify_${os}_${arch}.tar.gz";
-          sha256 = hashes."${os}-${arch}";
+          url = "https://github.com/arttor/helmify/releases/download/v0.4.17/helmify_${system}.tar.gz";
+          sha256 = hashes.${system};
         };
       phases = ["installPhase" "patchPhase"];
       installPhase = ''
