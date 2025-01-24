09fcb1126c6e2fe13bf0eaa2f9a3d654beab2ed3
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2025-01-24 21:49:23 +0100

    chore: remove unused code and echos

--- manifests/disable-ap.sh
+++ manifests/disable-ap.sh
@@ -5,22 +5,13 @@ CONNECTION_NAME="$WLAN_INTERFACE-ap"
 
 # Check if the connection exists
 if nmcli connection show "$CONNECTION_NAME" > /dev/null 2>&1; then
-    echo "Disabling connection '$CONNECTION_NAME'..."
-    # Bring the connection down if it's active
+    # Bring the connection down if it is active
     if nmcli connection show --active | grep -q "$CONNECTION_NAME"; then
         nmcli connection down "$CONNECTION_NAME"
-        echo "Connection '$CONNECTION_NAME' has been deactivated."
-    else
-        echo "Connection '$CONNECTION_NAME' is not active."
     fi
 
     # Remove the connection entirely
-    echo "Removing connection '$CONNECTION_NAME'..."
     nmcli connection delete "$CONNECTION_NAME"
-    echo "Connection '$CONNECTION_NAME' has been removed."
-else
-    echo "Connection '$CONNECTION_NAME' does not exist. Nothing to disable or remove."
 fi
 
-echo "Access point has been disabled and removed."
 killall sleep
